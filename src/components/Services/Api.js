e4e7aed122a62acaa9c97c6f01deed23fb6038a3
Prince <princekumar668348@gmail.com>
2023-08-21 12:51:06 +0530

    yes

--- src/components/Services/Api.js
+++ src/components/Services/Api.js
@@ -30,6 +30,7 @@ const Fetchapi = () => {
                 <img src={item.urlToImage} class="card-img-top" alt="..." />
                 <div className="card-body">
                   <h5 className="card-title">{item.source.name}</h5>
+
                   <h4 className="card-auth">{item.author}</h4>
 
                   <p className="card-text">{item.title}</p>
